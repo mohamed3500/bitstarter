65c69f285672a920950296d2d9f1700fe60b0c04
mohamed3500 <mohamed3500@gmail.com>
2013-07-19 18:45:58 +0200

    HW3 Part 1

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
 var express = require('express');
-
-var app = express.createServer(express.logger());
-
-var buffer = new buffer(16); 
-
 var fs = require('fs');
 
-var http = require('http'),
+var data = fs.readFileSync("index.html", "utf-8");
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+var app = express.createServer(express.logger());
+app.get('/', function(request, response) {
+  response.send(data);
 });
-
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
