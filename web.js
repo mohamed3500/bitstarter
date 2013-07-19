4eda373ffaaca72c22a742089f087f109e95c929
mohamed3500 <mohamed3500@gmail.com>
2013-07-19 18:39:05 +0200

    another change

--- web.js
+++ web.js
@@ -2,8 +2,21 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = new buffer(16); 
+
+var fs = require('fs');
+
+var http = require('http'),
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
 
 var port = process.env.PORT || 5000;
